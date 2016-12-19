8a7cd53bcc3d122f3e85d6ff1be477e80a098fe9
Alexandre Caulier <alexandre.caulier@orange.fr>
2016-12-19 16:59:29 +0100

    Bug fix to have a random agent

--- starter.py
+++ starter.py
@@ -1,11 +1,16 @@
 import gym
 import universe  # register the universe environments
+from universe import wrappers
 
-env = gym.make('flashgames.DuskDrive-v0')
+env = gym.make('gym-core.Pong-v3')
+env = wrappers.SafeActionSpace(env)
+
 env.configure(remotes=1)  # automatically creates a local docker container
+
 observation_n = env.reset()
 
+
 while True:
-  action_n = [[('KeyEvent', 'ArrowUp', True)] for ob in observation_n]  # your agent here
+  action_n = [env.action_space.sample() for ob in observation_n]  # your agent here
   observation_n, reward_n, done_n, info = env.step(action_n)
   env.render()
